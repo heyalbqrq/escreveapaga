e2e5dc42a7c05d301acaf722da08f53a7b3e2454
Wash Albuquerque <30643662+heyalbqrq@users.noreply.github.com>
2019-09-30 09:52:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 /* Nada por aqui, por enquanto 👾*/
+
+$(function () {
+
+$('.showButton').click(function () {
+
+$('.hidden').show();
+
+$('.show').hide();
+
+});
+
+$('.hideButton').click(function () {
+
+$('.hidden').hide();
+
+$('.show').show();
+
+});
+
+});
