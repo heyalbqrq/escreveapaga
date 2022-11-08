ab7ec3fca773f5bc3f0c26965e39b38256a99f64
Glitch (escreve-apaga) <none>
2022-11-08 16:03:21 +0000

    🟠🏙️ Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 /* Nada por aqui, por enquanto 👾*/
+
+// dfeDark Mode
+
+$( ".inner-switch" ).on("click", function() {
+    if( $( "body" ).hasClass( "dark" )) {
+      $( "body" ).removeClass( "dark" );
+      $( ".inner-switch" ).text( "🌚" );
+    } else {
+      $( "body" ).addClass( "dark" );
+      $( ".inner-switch" ).text( "🌞" );
+    }
+}); 
+function darkMode() {
+        var element = document.body;
+        element.classList.toggle("dark-mode");
+      }
